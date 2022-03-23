c59cec7b6ab445005c8d4ccfbea0856cabb9525e
Sidhin S Thomas <thomas.sidhin@outlook.com>
2022-03-23 08:43:23 +0530

    Overview, Experience done.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,11 +3,9 @@ import {Header} from './components/header'
 import {Body} from './components/body'
 
 import React from 'react';
-import { Layout, Menu, Breadcrumb, Image, Grid, Row } from 'antd';
+import {} from 'antd';
 
-import {Sections, profile, name, picUrl, socialUrls, sections} from './data.json';
-
-const { Content, Footer } = Layout;
+import {profile, name, picUrl, socialUrls, sections} from './data.json';
 
 function App () 
 {
@@ -15,6 +13,8 @@ function App ()
     <>
         <Header profile={profile} name={name} picUrl={picUrl} socialUrls={socialUrls}/>
         <Body sections={sections}/>
+        <h6>Designed by Sidhin S Thomas copyright 2022</h6>
+        <h6><a href='https://www.freepik.com/vectors/background'>Background vector created by rawpixel.com - www.freepik.com</a></h6>
     </>
   );
 }
