3176cb40c03daf3c30324decbcf18546c4ea3a9f
Sidhin S Thomas <thomas.sidhin@outlook.com>
2022-04-02 00:38:13 +0530

    Added Footer

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,6 +5,7 @@ import {Body} from './components/body'
 import React from 'react';
 
 import {profile, name, picUrl, socialUrls, sections} from './data.json';
+import { Footer } from './components/footer';
 
 function App () 
 {
@@ -13,10 +14,7 @@ function App ()
         <div id='sticky-bg'></div>
         <Header profile={profile} name={name} picUrl={picUrl} socialUrls={socialUrls}/>
         <Body sections={sections}/>
-        {/* <div className='footer'>
-          <h6>Designed by Sidhin S Thomas copyright 2022</h6>
-           <h6><a href='https://www.freepik.com/vectors/background'>Background vector created by rawpixel.com - www.freepik.com</a></h6>
-        </div> */}
+        <Footer/>
     </>
   );
 }
---
+++ portfolio/src/components/footer.jsx
@@ -0,0 +1,16 @@
+import { Layout } from "antd";
+import React from "react";
+
+export function Footer(props) 
+{
+    return (
+        <div id="footer">
+            <span>
+                Copyright ©️ Sidhin S Thomas
+            </span>
+            <span>
+            Background vector created by rawpixel.com
+            </span>
+        </div>
+    );
+}
