8b7db23dfc7e9b935a923a18234d124d13c22c9f
Sidhin S Thomas <thomas.sidhin@outlook.com>
2022-04-01 12:42:00 +0530

    Updated education information

--- portfolio/src/components/education.jsx
+++ portfolio/src/components/education.jsx
@@ -9,10 +9,11 @@ export function Education(props)
         <Collapse tabPosition='left' style={{width: '60vw'}} bordered={false} 
             defaultActiveKey={[...Array(content.length).keys()]} ghost>
         {content.map((detail,index) =>
-            <Collapse.Panel header={detail.title} key={index} showArrow={false}>
+            <Collapse.Panel header={detail.header} key={index} showArrow={false}>
                 <Card bordered={true} className='section-card'>
-                    <h2>{detail.title}</h2>
-                    <h4>{detail.school}</h4>
+                    <h2>{detail.school}</h2>
+                    <h4>{detail.course}</h4>
+                    <h5>{detail.timeline}</h5>
                 </Card>
             </Collapse.Panel>
         )}
