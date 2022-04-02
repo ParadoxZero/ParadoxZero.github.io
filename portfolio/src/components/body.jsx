0ebc28aa6b9e9f088cd27b46ad519c058139710a
Sidhin S Thomas <thomas.sidhin@outlook.com>
2022-04-02 20:30:54 +0530

    Scroll tab switcher

--- portfolio/src/components/body.jsx
+++ portfolio/src/components/body.jsx
@@ -6,17 +6,75 @@ import {Overview} from './overview';
 import { ErrorNotFound } from './ErrorNotFound';
 import { Experience } from './experience';
 
+const DEBOUNCE_TIME_DELTA = 500;
+
 export class Body extends React.Component
 {
     state = {
         current: {}
     };
 
+    scroll_debounce_timestamp = 0;
+
     constructor(props)
     {
         super(props);
         this.state.current = props.sections[0];
     }
+
+    componentDidMount() 
+    {
+        let root = document.getElementById('root');
+        root.addEventListener( 'wheel', this.handleScrollGesture);        
+    }
+
+    componentWillUnmount() {
+        let root = document.getElementById('root');
+        root.removeEventListener('wheel', this.handleScrollGesture);
+    }
+
+    handleScrollGesture = (ev) => {
+        //console.log(ev);
+        if(Date.now() - this.scroll_debounce_timestamp > DEBOUNCE_TIME_DELTA)
+        {
+            if(ev.deltaY > 0) //Going down
+            {
+                if (this.isScrollAtBottom()) {
+                    this.moveSection(1);
+                }
+            }
+            else // going up
+            {
+                if (this.isScrollAtTop()) {
+                        
+                    this.moveSection(-1);
+                }
+            }
+            this.scroll_debounce_timestamp = Date.now();
+        }
+    }
+
+    isScrollAtBottom() {
+        let root = document.getElementById('root');
+        //console.log(window.innerHeight, window.scrollY, root.scrollHeight)
+        return root.scrollHeight - (window.innerHeight + window.scrollY) <= 10;
+    }
+
+    isScrollAtTop() {
+        let root = document.getElementById('root');
+        //console.log(window.innerHeight, window.scrollY, root.scrollHeight)
+        return window.scrollY <= 10;
+    }
+
+    moveSection = (step) => {
+        console.log(this.state)
+        if(this.state.current)
+        {
+            const current_index = this.findIndex(this.state.current.header);
+            const next_index = (current_index + 1*step + this.props.sections.length) % this.props.sections.length;
+            this.setState({ current:this.props.sections[next_index] });
+        }
+    };
 
     findIndex = key => {
         const max = this.props.sections.length;
