0407e9e50465bff0342804bf504e95ec27608afd
Sidhin S Thomas <thomas.sidhin@outlook.com>
2022-04-02 16:25:41 +0530

    added Contact me card

--- portfolio/src/components/header.jsx
+++ portfolio/src/components/header.jsx
@@ -1,4 +1,4 @@
-import { GithubFilled, InstagramFilled, MailOutlined, LinkedinFilled } from "@ant-design/icons";
+import { GithubFilled, InstagramFilled, MailOutlined, LinkedinFilled, PhoneOutlined, PhoneFilled, MailFilled } from "@ant-design/icons";
 import { Card, Image, Space } from "antd";
 import React from "react";
 
@@ -21,6 +21,18 @@ export class Header extends React.Component
                         <a href={this.props.socialUrls.github}><GithubFilled/></a>
                         <a href={this.props.socialUrls.linkedin}><LinkedinFilled/></a>
                         <a href={this.props.socialUrls.instagram}><InstagramFilled/></a>
+                    </Space>
+                </Space>
+            </div>
+            <div className="contact-container" >
+                <Space direction="vertical" size='middle'>
+                    <Space direction="horizontal">
+                        <MailFilled/>
+                        <span>{this.props.socialUrls.email}</span>
+                    </Space>
+                    <Space direction="horizontal">
+                        <PhoneFilled rotate={100}/>
+                        <span>{this.props.socialUrls.phone}</span>
                     </Space>
                 </Space>
             </div>
