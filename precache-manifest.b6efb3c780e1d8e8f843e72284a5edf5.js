7545d8d66c1e671d5714ea16cb1822d955538e63
Sidhin S Thomas <thomas.sidhin@outlook.com>
2022-04-03 11:16:37 +0530

    Updates

---
+++ precache-manifest.b6efb3c780e1d8e8f843e72284a5edf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a47c85fd9053b8cb323",
+    "url": "/static/css/main.4a939750.chunk.css"
+  },
+  {
+    "revision": "0a47c85fd9053b8cb323",
+    "url": "/static/js/main.b825c7d3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "27f4c8ce7aaa77d89f33",
+    "url": "/static/css/2.445dcf38.chunk.css"
+  },
+  {
+    "revision": "27f4c8ce7aaa77d89f33",
+    "url": "/static/js/2.1d6bc7de.chunk.js"
+  },
+  {
+    "revision": "ef21896aa43f46431f8186179b4ba803",
+    "url": "/index.html"
+  }
+];
